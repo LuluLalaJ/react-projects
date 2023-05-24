dcf2f00b598e8927309c3e32e0df95f2ededf845
Chen <chen.jiang.1206@gmail.com>
2023-05-24 19:40:51 -0400

    practice done

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -4,8 +4,84 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [tours, setTours] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  function deleteTour(id) {
+    const updatedTours = tours.filter(tour => tour.id !== id)
+    setTours(updatedTours)
+  }
+
+  //modify later to use async func syntax
+  // useEffect(()=>{
+  //   fetch(url)
+  //   .then(r => r.json())
+  //   .then(data => {
+  //     setLoading(false)
+  //     setTours(data)
+  //   })
+  // }, [])
+
+  async function fetchTours() {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch(error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  // const fetchTours = async () => {
+  //   setLoading(true)
+  //   try {
+  //     const response = await fetch(url)
+  //     const tours = await response.json()
+  //     setLoading(false)
+  //     setTours(tours)
+  //   } catch (error) {
+  //     setLoading(false)
+  //     console.log(error)
+  //   }
+  // }
+
+  useEffect(()=>{
+    fetchTours()
+  }, [])
+
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={()=>fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+
+  return(
+    <main>
+      <Tours tours={tours} deleteTour={deleteTour}/>
+    </main>
+
+  )
 }
 
 export default App
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({name, info, image, price, id, deleteTour}) => {
+  const [isShort, setIsShort] = useState(true)
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {isShort
+          ? `${info.substring(0, 200)}...`
+          : info}
+          <button className="info-btn" onClick={()=>setIsShort(!isShort)}>{isShort?'read more':'read less'}</button>
+        </p>
+        <button className="delete-btn" onClick={(e)=>deleteTour(id)}>not interested</button>
+      </footer>
+    </article>
+  )
 };
 
 export default Tour;
