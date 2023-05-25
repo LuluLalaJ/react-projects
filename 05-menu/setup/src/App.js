00247ab2228b100882b04fde3bc1c654b00e7ea8
Chen <chen.jiang.1206@gmail.com>
2023-05-25 09:16:39 -0400

    practice done

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,7 +4,44 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menu, setMenu] = useState(items)
+  const [category, setCategory] = useState('all')
+
+  const renderedMenuItems = menu.filter(item => {
+    if (category === 'all') {
+      return true
+    } else {
+      return item.category === category
+    }
+    // return (true || item.category === category)
+  })
+
+  const filterByCategory = (category) => {
+    setCategory(category)
+  }
+
+  const renderMenu = renderedMenuItems.map(item => <Menu key={item.id} {...item}/>)
+
+  return(
+    <main>
+      <section className="menu">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="btn-container">
+
+          <button type="button" className="filter-btn" onClick={(e)=>filterByCategory(e.target.textContent)}>all</button>
+          <button type="button" className="filter-btn" onClick={(e)=>filterByCategory(e.target.textContent)}>breakfast</button>
+          <button type="button" className="filter-btn" onClick={(e)=>filterByCategory(e.target.textContent)}>lunch</button>
+          <button type="button" className="filter-btn" onClick={(e)=>filterByCategory(e.target.textContent)}>shakes</button>
+        </div>
+        <div className="section-center">
+          {renderMenu}
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default App;
