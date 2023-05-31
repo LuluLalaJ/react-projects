99b93916f772d3ca59280e18cba0a469cd121505
Chen <chen.jiang.1206@gmail.com>
2023-05-31 18:03:41 -0400

    done

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,36 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [text, setText] = useState([])
+  const [numOfParagraph, setNumOfParagraph] = useState(1)
+
+  const paragraphDisplayed = text
+
+  const generateText = (e) => {
+    e.preventDefault();
+    let num = parseInt(numOfParagraph)
+    setText(data.slice(0, num))
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className="section-center">
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className="lorem-form" onSubmit={e => generateText(e)}>
+      <label htmlFor="amount">paragraphs</label>
+      <input type="number" name="amount" id="amount" min="1"
+      step="1" max="8" value={numOfParagraph} onChange={(e)=>setNumOfParagraph(e.target.value)
+      }
+      />
+      <button className="btn">generate</button>
+      <article className="result">
+        {text.map( paragraph => {
+          return(
+            <p>{paragraph}</p>
+          )
+        })}
+      </article>
+    </form>
+  </section>
     )
 }
 
